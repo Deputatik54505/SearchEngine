7d0336658be253b20ae4ec46ace8328a6e2a906d
Иван <platonov.vania0-0@yandex.ru>
2023-08-24 18:10:40 +0300

    Page model created

---
+++ Data/Models/Page.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Xml.Linq;
+
+namespace SearchEngine.Data.Models;
+
+public class Page
+{
+    public Page(string url, string site, string text)
+    {
+        Url = url;
+        Site = site;
+        Text = text;
+        LastUpdate = DateOnly.FromDateTime(DateTime.Now);
+    }
+    public string Url { get; set; }
+    public string Site { get; set; }
+    public string Text { get; set; }
+    public DateOnly LastUpdate { get; set; }
+}
---
+++ Data/Models/Site.cs
@@ -0,0 +1,30 @@
+﻿namespace SearchEngine.Data.Models;
+
+/// <summary>
+/// Model of website.
+/// </summary>
+public class Site
+{
+
+    public Site(string ipAddress, string name, string url)
+    {
+        IpAddress = ipAddress;
+        Name = name;
+        Url = url;
+        LastUpdate = DateOnly.FromDateTime(DateTime.Now);
+    }
+
+    /// <summary>
+    /// Actual IP address of website
+    /// </summary>
+    public string IpAddress { get; set; }
+
+    /// <summary>
+    /// Name of website for search engine
+    /// </summary>
+    public string Name { get; set; }
+
+    public string Url { get; set; }
+
+    public DateOnly LastUpdate { get; set; }
+}
